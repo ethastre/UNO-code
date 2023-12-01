e12f5100b068e1ca7317bf660e8127389aeb1102
ethastre <ethastre@woodland.on.ca>
2023-12-01 10:02:41 -0500

    UNO Code

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import random
 
-print("Welcome to Uno, How Many People Are Playing?")
-NumPlayers = input()
+print("Welcome to Uno")
 Colors = ['Red', 'Green', 'Blue', 'Yellow']
-deck= random.choice(Colors), random.randint(0,9)
+Deck = []
+Hand1 = []
+Hand2 = []
+Discard = []
+for i in range(0,108):
+   Deck.append ([random.choice(Colors), random.randint(0,9)])
+for i in range(0,7):
+    Hand1.append(random.choice(Deck))
+for i in range(0,7):
+    Hand2.append(random.choice(Deck))
+Discard.append(random.choice(Deck))
 
+print("The Top Of Discard is"+)
