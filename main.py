c151b9339e91b210fde9d668a039dcab59cc114e
ethastre <ethastre@woodland.on.ca>
2023-12-01 09:21:54 -0500

    ok good

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+print("Welcome to Uno, How Many People Are Playing?")
+NumPlayers = input()
+Colors = ['Red', 'Green', 'Blue', 'Yellow']
+deck= random.choice(Colors), random.randint(0,9)
+
