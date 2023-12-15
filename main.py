f50bdaad2a3d051c53fd9ffd4f24b842973e6b1d
ethastre <ethastre@woodland.on.ca>
2023-12-15 09:53:14 -0500

    23/12/15

--- main.py
+++ main.py
@@ -6,12 +6,48 @@ Deck = []
 Hand1 = []
 Hand2 = []
 Discard = []
-for i in range(0,108):
-   Deck.append ([random.choice(Colors), random.randint(0,9)])
-for i in range(0,7):
-    Hand1.append(random.choice(Deck))
-for i in range(0,7):
-    Hand2.append(random.choice(Deck))
-Discard.append(random.choice(Deck))
 
-print("The Top Of Discard is"+)
+def turn1():
+    print("The Top Of Discard is", Discard)
+    print("Player 1 your hand is", Hand1)
+    print("Your Turn, play a card that matches the discard, if none match type 'none'")
+    Position = int(input())
+
+    if Hand1[Position][0] == Discard[0][0] or Hand1[Position][1] == Discard[0][1]:
+        Discard.append(Hand1.pop(Position))
+        print(Discard[-1])
+        print(Hand1)
+        if Hand1[Position][0] != Discard[0][0] and Hand1[Position][1] != Discard[0][1]:
+            Hand1.append(Deck.pop())
+            print('Your hand is now', Hand1)
+
+
+
+def turn2():
+    print("The Top Of Discard is", Discard)
+    print("Player 2 your hand is", Hand2)
+    print("Type the position of the card starting at zero, if none match type 'none'")
+    position =int(input())
+    if Hand2[position][0] == Discard[0][0] or Hand2[position][1] == Discard[0][1]:
+        Discard.append(Hand2.pop(position))
+        if Hand2[position][0] != Discard[0][0] and Hand2[position][1] != Discard[0][1]:
+            Hand2.append(Deck.pop())
+            print('Your hand is now', Hand2)
+
+for h in range(0,2):
+    for color in Colors:
+        for i in range(0,10):
+            Deck.append([color, str(i)])
+random.shuffle(Deck)
+for i in range(0,7):
+    Hand1.append(Deck.pop())
+for i in range(0,7):
+    Hand2.append(Deck.pop())
+Discard.append(Deck.pop())
+
+
+while len(Hand1)>0 and len(Hand2)>0:
+    turn1()
+    turn2()
+    if len(Hand1)==0:
+        break
