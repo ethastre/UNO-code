e92c3b57503fb328a2c604947a8fd204af359881
ethastre <ethastre@woodland.on.ca>
2024-01-08 12:13:24 -0500

    24/01/08

--- main.py
+++ main.py
@@ -20,32 +20,35 @@ Discard.append(Deck.pop())
 def turn1():
     print("The Top Of Discard is", Discard[-1])
     print("Player 1 your hand is", Hand1)
-    print("Type the position of the card starting at zero, if none match type 'none'")
+    print("Type the position of the card starting at zero, if none match type '0'")
     position = int(input())
-    if Hand1[position][0] != Discard[0][0] and Hand1[position][1] != Discard[0][1]:
-        print(Hand1[position][0],Hand1[position][1],Discard[0][0],Discard[0][1])
-        Hand1.append(Deck.pop())
-        print('No Match')
-    if Hand1[position][0] == Discard[0][0] or Hand1[position][1] == Discard[0][1]:
-        Discard.append(Hand1.pop(position))
+    if Hand1 is not None:
+        if Hand1[position][0] != Discard[-1][0] and Hand1[position][1] != Discard[-1][1]:
+            Hand1.append(Deck.pop())
+            print('No Match')
+        if Hand1[position][0] == Discard[-1][0] or Hand1[position][1] == Discard[-1][1]:
+            Discard.append(Hand1.pop(position))
     print('Your hand is now', Hand1)
 
 def turn2():
     print("The Top Of Discard is", Discard[-1])
     print("Player 2 your hand is", Hand2)
-    print("Type the position of the card starting at zero, if none match type 'none'")
+    print("Type the position of the card starting at zero, if none match type '0'")
     position =int(input())
-    if Hand2[position][0] != Discard[-1][0] and Hand2[position][1] != Discard[-1][1]:
-        print(Hand1[position][0], Hand1[position][1], Discard[0][0], Discard[0][1])
-        Hand2.append(Deck.pop())
-        print('No Match')
-    if Hand2[position][0] == Discard[-1][0] or Hand2[position][1] == Discard[-1][1]:
-        Discard.append(Hand2.pop(position))
-    print('Your hand is now', Hand2)
+    if Hand2 is not None:
+        if Hand2[position][0] != Discard[-1][0] and Hand2[position][1] != Discard[-1][1]:
+            Hand2.append(Deck.pop())
+            print('No Match')
+        if Hand2[position][0] == Discard[-1][0] or Hand2[position][1] == Discard[-1][1]:
+            Discard.append(Hand2.pop(position))
+
 
 while len(Hand1)>0 and len(Hand2)>0:
     turn1()
-    turn2()
     if len(Hand1)==0:
+        print("Congrats Player 1, you won!")
         break
-print ("Congrats, you won!")
+    turn2()
+    if len(Hand2)==0:
+        print("Congrats Player 2, you won!")
+        break
