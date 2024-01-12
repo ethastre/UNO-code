23f5e0065d7725870f95a0e2b7c8c3206490cfdb
ethastre <ethastre@woodland.on.ca>
2024-01-12 10:00:19 -0500

    24/01/12

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ Hand1 = []
 Hand2 = []
 Discard = []
 for h in range(0,2):
+   for color in Colors:
+       Deck.append([color,"+2"])
+
+for h in range(0,2):
     for color in Colors:
         for i in range(0,10):
             Deck.append([color, str(i)])
+
 random.shuffle(Deck)
 for i in range(0,7):
     Hand1.append(Deck.pop())
@@ -26,9 +31,11 @@ def turn1():
         if Hand1[position][0] != Discard[-1][0] and Hand1[position][1] != Discard[-1][1]:
             Hand1.append(Deck.pop())
             print('No Match')
+        if Hand1[position][0] == Discard[-1][0] and Hand1[position][1] == "+2":
+            for i in range(0, 2):
+                Hand2.append(Deck.pop())
         if Hand1[position][0] == Discard[-1][0] or Hand1[position][1] == Discard[-1][1]:
             Discard.append(Hand1.pop(position))
-    print('Your hand is now', Hand1)
 
 def turn2():
     print("The Top Of Discard is", Discard[-1])
@@ -39,10 +46,14 @@ def turn2():
         if Hand2[position][0] != Discard[-1][0] and Hand2[position][1] != Discard[-1][1]:
             Hand2.append(Deck.pop())
             print('No Match')
+        if Hand2[position][0] == Discard[-1][0] and Hand2[position][1] == "+2":
+            for i in range(0,2):
+                Hand1.append(Deck.pop())
         if Hand2[position][0] == Discard[-1][0] or Hand2[position][1] == Discard[-1][1]:
             Discard.append(Hand2.pop(position))
 
 
+print(Deck)
 while len(Hand1)>0 and len(Hand2)>0:
     turn1()
     if len(Hand1)==0:
